d133ff4ef773ead9428374e1ed4fe1b0af51bd04
Enuch <enuchsantys@gmail.com>
2022-12-25 16:20:59 -0300

    Initial commit

---
+++ src/main/java/edu/enuch/tipos/Primitivos.java
@@ -0,0 +1,21 @@
+package edu.enuch.tipos;
+
+public class Primitivos {
+    /*
+    * byte   = 1 byte  | -128 a 127
+    * short  = 2 bytes | -32.768 a 32.767
+    * int    = 4 bytes | -2.147.483.648 a 2.147.483.648
+    * long   = 8 bytes | -9.223.372.036.854.775.808 a 9.223.372.036.854.775.808
+    * float  = 4 bytes | -3.4028E+38 a 3.4028E+38
+    * double = 8 bytes | -1.7976E+308 a 1.7976E+308
+    */
+
+    // Exemplos de uso
+    byte idade = 15;
+    short ano = 2022;
+    int cep = 59104240;
+    long cpf = 70166845477L;
+    float pi = 3.14F;
+
+    // Java é fortemente tipado
+}
---
+++ src/main/java/edu/enuch/variaveis/Constantes.java
@@ -0,0 +1,12 @@
+package edu.enuch.variaveis;
+
+public class Constantes {
+    public static void main(String[] args) {
+        // Váriaveis
+        short ano = 2022;
+        ano = 2050;
+
+        // Constantes
+        final float PI = 3.14F;
+    }
+}
