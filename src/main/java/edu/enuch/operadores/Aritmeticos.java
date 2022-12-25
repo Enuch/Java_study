f4e7bfc996618314cea901f7a47b0a93595cce54
Enuch <enuchsantys@gmail.com>
2022-12-25 17:12:32 -0300

    Operadores aritméticos

---
+++ src/main/java/edu/enuch/operadores/Aritmeticos.java
@@ -0,0 +1,36 @@
+package edu.enuch.operadores;
+
+public class Aritmeticos {
+    public static void main(String[] args) {
+
+        // Operadores unários e aritméticos
+        int soma = 2 + 2;
+        int subtrai = 2 - 2;
+        int multiplica = 2 * 2;
+        int dividi = 2 / 2;
+        int modulo = 2 % 2;
+
+        // Operador de soma é usado para concatenar
+        String concatena = "Enuch " + "Santos";
+
+        // Incrementa ou decrementa após ler a váriavel
+        int incrementa = 10;
+        incrementa++;
+        int decrementa = 10;
+        decrementa--;
+
+        // Incrementa ou decrementa antes de ler a váriavel
+        ++incrementa;
+        --incrementa;
+
+        // Mudando o valor de um número para positivo ou negativo
+        int numero = 10;
+        // valor negativo
+        numero = -numero;
+        System.out.println(numero);
+        // valor positivo
+        numero = -numero;
+        System.out.println(numero);
+
+    }
+}
